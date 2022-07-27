ed291791059524c6cdad3708a82b3437bbd47d3f
Alessandragr <alessandra-ribeiro@live.com>
2022-07-27 10:52:30 -0300

    Atividade 4

---
+++ 0x00/salvando/sql_schema_first_db.sql
@@ -0,0 +1,15 @@
+-- TABLE
+CREATE TABLE CLIENTE (
+id integer autoincrement not null,
+nome varchar(80) not null,
+email varchar(40) not null,
+telefone integer,
+primary key (id)
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
